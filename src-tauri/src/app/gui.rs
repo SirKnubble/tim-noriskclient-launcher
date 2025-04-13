ca5aaab2cb1ff9a73210e7cb2b0ccd6424a928e4
Tim Lohrer <me@timlohrer.de>
2025-04-13 19:13:49 +0200

    feat: initialize js plugins

--- src-tauri/src/app/gui.rs
+++ src-tauri/src/app/gui.rs
@@ -2406,8 +2406,12 @@ pub fn gui_main() {
             }
             _ => {}
         })
-        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_notification::init())
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_shell::init())
         .setup(|app| {
             NRCCache::initialize_app_state(app);
             Ok(())
